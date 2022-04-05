ea5532128d19967931cdc03fbdeae54005e3ff67
eudycolmenares <eudy.developer@gmail.com>
2022-04-05 15:52:57 -0500

    HeroList component added

--- src/components/dc/DcScreen.jsx
+++ src/components/dc/DcScreen.jsx
@@ -1,7 +1,12 @@
+import { HeroList } from "../hero/HeroList"
+// 
 export const DcScreen = () => {
+    const publisher = 'DC Comics'
     return (
         <>
             <h1>Dc Screen</h1>
+            <hr />
+            <HeroList publisher={ publisher } />
         </>
     )
 }
--- src/components/marvel/MarvelScreen.jsx
+++ src/components/marvel/MarvelScreen.jsx
@@ -1,7 +1,12 @@
+import { HeroList } from "../hero/HeroList"
+// 
 export const MarvelScreen = () => {
+    const publisher = 'Marvel Comics'
     return (
         <>
             <h1>Marvel Screen</h1>
+            <hr />
+            <HeroList publisher={ publisher } />
         </>
     )
 }

6898b9e084d932e528df0c061486f5ae428ba85a
eudycolmenares <eudy.developer@gmail.com>
2022-04-14 19:10:09 -0500

    Private Routes are added

--- src/routers/AppRouter.jsx
+++ src/routers/AppRouter.jsx
@@ -1,12 +1,20 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { LoginScreen } from '../components/login/LoginScreen'
 import { DashboardRouter } from './DashboardRouter'
+import { PrivateRoute } from './PrivateRoute'
 // 
 export const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={ <LoginScreen /> } />
+                <Route path='/*' element=
+                    {
+                        <PrivateRoute>
+                            <DashboardRouter />
+                        </PrivateRoute>
+                    } 
+                />
                 <Route path='/*' element={ <DashboardRouter /> } />
             </Routes>
         </BrowserRouter>
---
+++ src/routers/PrivateRoute.jsx
@@ -0,0 +1,11 @@
+import { useContext } from "react"
+import { Navigate } from "react-router-dom"
+import { AuthContext } from "../auth/AuthContext"
+// 
+export const PrivateRoute = ({ children }) => {
+    const { user } = useContext( AuthContext)
+    return (user.logged 
+        ? children
+        : <Navigate to="login" />
+    )
+}
